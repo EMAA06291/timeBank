166c8ac3b4a98adb12aef861e7772f8b64b0dc7a
EMAA06291 <emyyy212@gmail.com>
2025-05-11 12:22:51 +0300

    try

--- js/faq.js
+++ js/faq.js
@@ -41,38 +41,38 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-closeNav();
+// closeNav();
 
-function openNav() {
-  $(".side-nav").animate({ left: 0 }, 500);
+// function openNav() {
+//   $(".side-nav").animate({ left: 0 }, 500);
 
-  $(".manue-i").addClass("fa-xmark");
-  $(".manue-i").removeClass("fa-bars");
-  $(".ul-nav li ").eq(0).animate({ top: 0 }, 500);
-  $(".ul-nav li ").eq(1).animate({ top: 0 }, 600);
-  $(".ul-nav li ").eq(2).animate({ top: 0 }, 700);
-  $(".ul-nav li ").eq(3).animate({ top: 0 }, 800);
-  $(".ul-nav li ").eq(4).animate({ top: 0 }, 900);
-  $(".ul-nav li ").eq(5).animate({ top: 0 }, 1000);
-  $(".ul-nav li ").eq(6).animate({ top: 0 }, 1100);
-  $(".ul-nav li ").eq(7).animate({ top: 0 }, 1200);
-  $(".ul-nav li ").eq(8).animate({ top: 0 }, 1300);
-  $(".ul-nav li ").eq(9).animate({ top: 0 }, 1400);
-  $(".ul-nav li ").eq(10).animate({ top: 0 }, 1500);
-  $(".ul-nav li ").eq(11).animate({ top: 0 }, 1600);
-}
-function closeNav() {
-  let navWidth = $(".rightSide").outerWidth();
+//   $(".manue-i").addClass("fa-xmark");
+//   $(".manue-i").removeClass("fa-bars");
+//   $(".ul-nav li ").eq(0).animate({ top: 0 }, 500);
+//   $(".ul-nav li ").eq(1).animate({ top: 0 }, 600);
+//   $(".ul-nav li ").eq(2).animate({ top: 0 }, 700);
+//   $(".ul-nav li ").eq(3).animate({ top: 0 }, 800);
+//   $(".ul-nav li ").eq(4).animate({ top: 0 }, 900);
+//   $(".ul-nav li ").eq(5).animate({ top: 0 }, 1000);
+//   $(".ul-nav li ").eq(6).animate({ top: 0 }, 1100);
+//   $(".ul-nav li ").eq(7).animate({ top: 0 }, 1200);
+//   $(".ul-nav li ").eq(8).animate({ top: 0 }, 1300);
+//   $(".ul-nav li ").eq(9).animate({ top: 0 }, 1400);
+//   $(".ul-nav li ").eq(10).animate({ top: 0 }, 1500);
+//   $(".ul-nav li ").eq(11).animate({ top: 0 }, 1600);
+// }
+// function closeNav() {
+//   let navWidth = $(".rightSide").outerWidth();
 
-  $(".side-nav").animate({ left: -navWidth }, 500);
-  $(".manue-i").removeClass("fa-xmark");
-  $(".manue-i").addClass("fa-bars");
-  $(".ul-nav li ").animate({ top: 500 }, 500);
-}
-$(".manue-i").click(() => {
-  if ($(".side-nav").css("left") == "0px") {
-    closeNav();
-  } else {
-    openNav();
-  }
-});
+//   $(".side-nav").animate({ left: -navWidth }, 500);
+//   $(".manue-i").removeClass("fa-xmark");
+//   $(".manue-i").addClass("fa-bars");
+//   $(".ul-nav li ").animate({ top: 500 }, 500);
+// }
+// $(".manue-i").click(() => {
+//   if ($(".side-nav").css("left") == "0px") {
+//     closeNav();
+//   } else {
+//     openNav();
+//   }
+// });
